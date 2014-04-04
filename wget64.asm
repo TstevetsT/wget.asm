b412929a2d7e638fde16bee1e9ddeab131f34744
tstevetst <root@localhost.localdomain>
2014-04-04 02:33:46 -0700

    wget64v0.5

--- wget64.asm
+++ wget64.asm
@@ -48,7 +48,6 @@ error:
 	mov rax, 4	;sys_write systemcall number
 	int 0x80
 
-
 exit:
 	pop rdi
 	pop rsi
@@ -134,7 +133,7 @@ GetURL:
 		.filenamedone:
 			xor rdi, rdi
 			.buildpath:
-				mov al, [ebx+ecx]
+				mov al, [rbx+rcx]
 				cmp al, 0x0
 				je .pathdone
 				mov [path+rdi], al
@@ -150,159 +149,134 @@ GetURL:
 	ret
 
 DNSLookup:		;2)  DNS Lookup to find IP, IP is .sin_addr in server
-	mov rax, url  
-	push rax
+	xor rax, rax
+	xor rdi, rdi
+	mov edi, url  
 	call resolv	;nslookup url
-	add rsp, 4
 	mov [server+sockaddr_in.sin_addr], eax
 	ret
 
-
 CreateSocket:           ;3)  Create Socket
-	push dword 0
-	push dword 1	;SOCK_STREAM
-	push dword 2	;AF_INET
-	mov rcx, rsp	;ecx must point at top of stack for sys_socket
-	mov rbx, 1	;sys_socket
-	mov eax, 0x66	;sys_socketcall
-	int 0x80   	;eax holds socket file descripter
-	add esp, 12	;resets stack leaving output file fd at top
-	mov esi, eax	;esi now holds socket fd
+	xor rdx, rdx
+	xor rsi, rsi
+	xor rdi, rdi
+	mov rdx, 2	;AF_INET (IPv4)
+	mov rsi, 1 	;SOCK_STREAM
+	mov rdi, 0	;int family
+	mov rax, 41	;sys_socket system call number
+	syscall
+	mov rsi, rax	;rsi now holds socket fd
 	ret
 
 MakeConnection:            ;4)  Establish Connection 
-	push dword [sockaddr_size]	;sockaddr size to stack
-	push server		;sockaddr struc address to stack
-	push esi		;socket fd to stack
-	mov ecx, esp		;top of stack to ecx
-	mov ebx, 3		;connect socketcall
-	mov eax, 0x66		;socketcall syscall # 102d=66x
-	int 0x80
-	add esp, 12
+	mov rdx, [sockaddr_size]	;sockaddr size to stack
+	mov rsi, server		;sockaddr struc address to stack
+	mov rdi, rsi		;socket fd to stack
+	mov rax, 42		;socketcall syscall # 102d=66x
+	syscall
 	ret	
 
 OpenFile:               ;5)  Open File named by last section of url
-	mov eax, 5  	;open sys call number
-	mov ebx, filename
-	mov ecx, 0o102  ;O_CREAT creates the file and makes it Read/Write
-	mov edx, 0o0666 ;Read and Write for user, group, and others
-	int 0x80   	;opens file and eax contains file descripter
-	mov edi, eax	;moves output file descriptor into edi
+	mov rdx, 0o0666	;Read and Write for user, group, and others
+	mov rsi, 0o102  ;O_CREAT creates the file and makes it Read/Write
+	mov edi, filename
+	mov rax, 2  	;open sys call number
+	syscall
+	mov rdi, rax	;moves output file descriptor into rdi
 	ret
 
 BuildGet:           ;get1 path get2 url get3       strlen
-	push esi
-	push edi
-	xor edi, edi
-	xor esi, esi
-	xor eax, eax
-	mov al, [get1+edi]
+	push rsi 
+	push rdi
+	xor rdi, rdi
+	xor rsi, rsi
+	xor rax, rax
+	mov al, [get1+rdi]
 	.get1loop:
 		cmp al,0
 		jz .addpath
-		mov [get+edi], al
-		inc edi
-		mov al, [get1+edi]
+		mov [get+rdi], al
+		inc rdi
+		mov al, [get1+rdi]
 		jmp .get1loop
 	.addpath:
-		mov al, [path+esi]
+		mov al, [path+rsi]
 		.pathloop:
 			cmp al, 0
 			jz .addget2
-			mov [get+edi], al
-			inc edi
-			inc esi
-			mov al, [path+esi]
+			mov [get+rdi], al
+			inc rdi
+			inc rsi
+			mov al, [path+rsi]
 			jmp .pathloop
 	.addget2:
-		xor esi, esi
-		mov al, [get2+esi]
+		xor rsi, rsi
+		mov al, [get2+rsi]
 		.get2loop:
 			cmp al, 0
 			jz .addurl
-			mov [get+edi], al
-			inc edi
-			inc esi
-			mov al, [get2+esi]
+			mov [get+rdi], al
+			inc rdi
+			inc rsi
+			mov al, [get2+rsi]
 			jmp .get2loop
 	.addurl:
-		xor esi, esi
-		mov al, [url+esi]
+		xor rsi, rsi
+		mov al, [url+rsi]
 		.urlloop:
 			cmp al, 0
 			jz .addget3
-			mov [get+edi], al
-			inc edi
-			inc esi
-			mov al, [url+esi]
+			mov [get+rdi], al
+			inc rdi
+			inc rsi
+			mov al, [url+rsi]
 			jmp .urlloop
 	.addget3:
-		xor esi, esi
-		mov al, [get3+esi]
+		xor rsi, rsi
+		mov al, [get3+rsi]
 		.get3loop:
 			cmp al, 0
 			jz .done
-			mov [get+edi], al
-			inc edi
-			inc esi
-			mov al, [get3+esi]
+			mov [get+rdi], al
+			inc rdi
+			inc rsi
+			mov al, [get3+rsi]
 			jmp .get3loop
 	.done:
-	mov [getlen], edi	
-	pop edi
-	pop esi
+	mov [getlen], rdi	
+	pop rdi
+	pop rsi
 	ret
 
 
 WriteFile:
-	mov edx, [getlen]	;Number of bytes to write
-	mov ecx, get
-	mov ebx, 1	;Writing to stdout
-	mov eax, 4	;sys_write systemcall number
-	int 0x80
+	mov rdx, [getlen]	;Number of bytes to write
+	mov rcx, get
+	mov rbx, 1	;Writing to stdout
+	mov rax, 1	;sys_write systemcall number
+	syscall
 
-	mov ebx, esi	;Writing to socket
-	mov eax, 4	;sys_write systemcall number
-	int 0x80
+	mov rbx, rsi	;Writing to socket
+	mov rax, 1	;sys_write systemcall number
+	syscall
 
-	mov edx, recbuflen
-	mov ecx, recbuf
-	mov ebx, esi
-	mov eax, 3	;Read From Socket
-	int 0x80
+	mov rdx, recbuflen
+	mov rcx, recbuf
+	mov rbx, rsi
+	mov rax, 0	;Read From Socket
+	syscall
 
-	mov edx, recbuflen
-	mov ecx, recbuf
-	mov ebx, edi
-	mov eax, 4
-	int 0x80
+;	mov rdx, recbuflen
+;	mov rcx, recbuf
+	mov rbx, rdi
+	mov rax, 1	;Write to Open File
+	syscall
 	ret
 
 CloseFile:
-	mov ebx, edi 	;File Descriptor for opened File
-	mov eax, 6      ;sys_close systemcall number ebx must be holding fd
+	mov rbx, rdi 	;File Descriptor for opened File
+	mov rax, 3      ;sys_close systemcall number rbx must be holding fd
 	ret
-
-strlen:          ;l_strlen(char *str)
-        ;returns the length of a null terminated string
-        push ebp
-        mov ebp, esp
-        push esi
-        push ecx
-        xor esi, esi
-        mov ecx, [ebp+8]
-        .loop:
-                cmp byte [ecx+esi], 0x0
-                je .done
-                add esi, 1
-                jmp .loop
-        .done:
-        mov eax, esi
-        pop ecx
-        pop esi
-        pop ebp
-        ret
-
 
 section .data
 server:	istruc sockaddr_in
